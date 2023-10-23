077aa0c6b941debb852a47eb46c5b72c3d6a1f53
gohyongjing <gohyongjing@gmail.com>
2023-10-23 23:04:18 +0800

    Fix matching service urls

--- microservices/front-end/vite.config.ts
+++ microservices/front-end/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), Svgr()],
   server: {
     proxy: {
-      "/api": process.env.AUTH_SERVICE_URL ?? "http://localhost:8080",
+      "/api": process.env.VITE_AUTH_SERVICE_URL ?? "http://localhost:8080",
     },
   },
 });
